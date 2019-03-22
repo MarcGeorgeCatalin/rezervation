2e9b98240d9537bac75a2f2c713a6b4c6129c85e
git config --global user.name John <marcgeorgecatalin@yahoo.com>
2019-03-22 13:55:03 +0200

    v2

---
+++ src/main/java/com/fortech/RezervationApplication.java
@@ -0,0 +1,13 @@
+package com.fortech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RezervationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RezervationApplication.class, args);
+	}
+
+}
